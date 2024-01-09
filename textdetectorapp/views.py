40cd71681ca378d7764ba08e3353083e088bbdb9
pvh261 <pvh2617@gmail.com>
2024-01-09 22:18:40 +0700

    commit

--- textdetectorapp/views.py
+++ textdetectorapp/views.py
@@ -2,39 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import os
-import requests
 import transformers
-
-class GPTZeroAPI:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = 'https://api.gptzero.me/v2/predict'
-
-    def text_predict(self, document):
-        url = f'{self.base_url}/text'
-        headers = {
-            'accept': 'application/json',
-            'X-Api-Key': self.api_key,
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'document': document
-        }
-        response = requests.post(url, headers=headers, json=data)
-        return response.json()
-
-    def file_predict(self, file_path):
-        url = f'{self.base_url}/files'
-        headers = {
-            'accept': 'application/json',
-            'X-Api-Key': self.api_key
-        }
-        files = {
-            'files': (os.path.basename(file_path), open(file_path, 'rb'))
-        }
-        response = requests.post(url, headers=headers, files=files)
-        return response.json()
+from pathlib import Path
 
 class Detector(View):
     def get(self, request):
@@ -49,7 +18,9 @@ class DetectorAPI(APIView):
 
         # some parameters
         DEVICE = "cpu"
-        model = rf'C:\Users\dungh\Documents\Detector\TextDetector\textdetectorapp\update_results\gpt2-medium-t5-base\Essay-ChatGPT\{method}'
+        path = Path.cwd()
+        model = path/'textdetectorapp'/'update_results'/'gpt2-medium-t5-base'/'Essay-ChatGPT'/method
+        print(model)
         num_labels = 2
         pos_bit = 1
 

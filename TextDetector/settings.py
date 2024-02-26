cc41e75df8401823a423ada961a3f0e64da4fcca
pvh261 <pvh2617@gmail.com>
2024-02-26 23:22:07 +0700

    yes

--- TextDetector/settings.py
+++ TextDetector/settings.py
@@ -107,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'textdetectorapp/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
